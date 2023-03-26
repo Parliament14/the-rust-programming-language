968824e47413f31bf108b5f57eef11728682da70
Austen <ajmoore14@gmail.com>
2023-03-26 17:21:24 -0500

    Adding more comments for posterity

--- projects/hello_world/main.rs
+++ projects/hello_world/main.rs
@@ -1,4 +1,5 @@
 fn main() { 
     // Using a macro rather than a function, denoted by the '!' at the end of "println!"
+    // To compile, use "rustc main.rs". To run, use "./main" like you would any other linux executable not in your path 
     println!("Hello, world!");
 }
